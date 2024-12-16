5f9f25a63a7ccc970924ffe094f86b702e684ac0
Ameen Nadeem <ameennadeem134@gmail.com>
2024-12-16 15:52:40 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
-let profileDropdownList = document.querySelector(".profile-dropdown-list");
-let btn = document.querySelector(".profile-dropdown-btn");
+ var responsiveSlider = function() {
 
-let classList = profileDropdownList.classList;
+var slider = document.getElementById("slider");
+var sliderWidth = slider.offsetWidth;
+var slideList = document.getElementById("slideWrap");
+var count = 1;
+var items = slideList.querySelectorAll("li").length;
 
-const toggle = () => classList.toggle("active");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
 
-window.addEventListener("click", function (e) {
-  if (!btn.contains(e.target)) classList.remove("active");
+window.addEventListener('resize', function() {
+  sliderWidth = slider.offsetWidth;
 });
+
+var prevSlide = function() {
+  if(count > 1) {
+    count = count - 2;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+  else if(count = 1) {
+    count = items - 1;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+};
+
+var nextSlide = function() {
+  if(count < items) {
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+  else if(count = items) {
+    slideList.style.left = "0px";
+    count = 1;
+  }
+};
+
+next.addEventListener("click", function() {
+  nextSlide();
+});
+
+prev.addEventListener("click", function() {
+  prevSlide();
+});
+
+setInterval(function() {
+  nextSlide()
+}, 5000);
+
+};
+
+window.onload = function() {
+responsiveSlider();
+}
